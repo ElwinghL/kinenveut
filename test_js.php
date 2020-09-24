0603631805eab6654b16eae4a324eb5916ed08bd
clement_1996@hotmail.com <clement_1996@hotmail.com>
2020-09-24 11:07:27 +0200

    Ajout d'un fichier test pour faire passer les variables PHP en JS

---
+++ test_js.php
@@ -0,0 +1,13 @@
+<?php
+
+$ma_variable = "";
+?>
+<div id="content"></div>
+<button onclick="displayJSONFromPHP()">Load JSON</button>
+<script>
+    function displayJSONFromPHP() {
+        const variablePHP = <?php json_encode($ma_variable); ?>;
+        document.getElementById("content").innerText = JSON.stringify(variablePHP, null, 1);
+    }
+</script>
+
