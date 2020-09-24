76be9dd99b967251270d0c019202f2049ac24c70
OphelieTrd <57994130+OphelieTrd@users.noreply.github.com>
2020-09-24 22:59:30 +0200

    Add: Init Data Base script

---
+++ creation_of_dataBase.sql
@@ -0,0 +1,63 @@
+DROP DATABASE IF EXISTS kinenveut;
+CREATE DATABASE kinenveut CHARACTER SET 'utf8';
+
+CREATE TABLE `kinenveut`.`Users` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`firtName` VARCHAR(100) NOT NULL,
+`lastName` VARCHAR(100) NOT NULL,
+`email` VARCHAR(255) NOT NULL,
+`password` VARCHAR(255) NOT NULL,
+`birthDate` DATE NOT NULL,
+`isAuthorised` INT DEFAULT NULL,
+`isAdmin` BOOLEAN,
+PRIMARY KEY (`id`),
+UNIQUE `Unicite_Mail` (`email`)
+) ENGINE = MyISAM;
+
+CREATE TABLE `kinenveut`.`Categories` (
+`id` SMALLINT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(100) NOT NULL,
+PRIMARY KEY (`id`),
+UNIQUE `Unicite_Name` (`name`)
+) ENGINE = MyISAM;
+
+CREATE TABLE `kinenveut`.`Objects` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(255) NOT NULL,
+`description` VARCHAR(255) NOT NULL,
+`basePrice` INT NOT NULL,
+`reservePrice` INT NOT NULL,
+`pictureLink` VARCHAR(255) NOT NULL,
+`startDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`endDate` DATETIME NOT NULL,
+`isCancelled` INT NULL,
+`sellerId` INT NOT NULL,
+`privacyId` INT NOT NULL,
+`categoryId` INT NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (sellerId) REFERENCES Users(id),
+FOREIGN KEY (categoryId) REFERENCES Categories(id),
+INDEX ind_name (name),
+INDEX ind_sellerId (sellerId),
+INDEX ind_categoryId (categoryId)
+) ENGINE = MyISAM;
+
+CREATE TABLE `kinenveut`.`BidHistory` (
+`id` INT NOT NULL AUTO_INCREMENT,
+`bidPrice` INT NOT NULL,
+`bidDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+`objectId` INT NOT NULL,
+`bidderId` INT NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (objectId) REFERENCES Objects(id),
+FOREIGN KEY (bidderId) REFERENCES Users(id),
+INDEX ind_bidderId (bidderId)
+) ENGINE = MyISAM;
+
+CREATE VIEW V_Privacy
+AS SELECT 0 AS id, 'public' AS name
+UNION SELECT 1 AS id, 'privée' AS name
+UNION SELECT 2 AS id, 'confidentielle' AS name
+
+--LOCK TABLES Race WRITE;
+--UNLOCK TABLES;
