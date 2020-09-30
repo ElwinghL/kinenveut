2fb3cde91100276a14f807cbe9e5df9eeb646ae9
OphelieTrd <ofeely-56@hotmail.fr>
2020-09-30 13:49:27 +0200

    [Couche BO] Ajout de la fonction getAllAuctionsByAuctionState qui retourne tous les objets avec le meilleur prix enchéri +Ajout du fichier de test à remplir Comment de la fonction selectAllAuctions devenue obsolète

--- src/service/bo/IAuctionBo.php
+++ src/service/bo/IAuctionBo.php
@@ -2,7 +2,9 @@
 
 interface IAuctionBo
 {
+  public function getAllAuctionsByAuctionState(int $auctionState) : array;
+
   public function insertAuction(AuctionModel $auction);
 
-  public function selectAllAuctions();
+  //public function selectAllAuctions();
 }
