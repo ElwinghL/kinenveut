ec657cdb4b0f3cee9b9f897d46ce655681b42b88
OphelieTrd <ofeely-56@hotmail.fr>
2020-10-02 21:09:52 +0200

    [DAO] Implémentation des fonctions d'ajout et de suppression des catégories [DAO] Création des tests relatifs au Category

---
+++ test/service/dao/CategoryDaoTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+$dotenv = Dotenv\Dotenv::createImmutable('.');
+$dotenv->load();
+include_once 'src/db.php';
+include_once 'src/tools.php';
+
+class CategoryDaoTest extends TestCase
+{
+  /** @before*/
+  public function setUp() : void
+  {
+    parent::setUp();
+    App_DaoFactory::setFactory(new App_DaoFactory());
+  }
+
+  /** @test */
+  public function insertCategoryTest() : void
+  {
+    $categoryDao = App_DaoFactory::getFactory()->getCategoryDao();
+
+    $categoryTest = new CategoryModel();
+    $categoryTest->setName('Test');
+
+    $categoryId = $categoryDao->insertCategory($categoryTest);
+
+    $this->assertNotNull($categoryId);
+
+    $categoryDao->deleteCategoryById($categoryId);
+  }
+
+  /** @test */
+  public function deleteCategoryTest() : void
+  {
+    $categoryDao = App_DaoFactory::getFactory()->getCategoryDao();
+    $categoryTest = new CategoryModel();
+    $categoryTest->setName('Test');
+
+    $categoryId = $categoryDao->insertCategory($categoryTest);
+
+    $success = $categoryDao->deleteCategoryById($categoryId);
+
+    $this->assertTrue($success);
+  }
+
+  /** @test */
+  public function selectAllCategoriesTest() : void
+  {
+    $categoryDao = App_DaoFactory::getFactory()->getCategoryDao();
+    $categoryTest = new CategoryModel();
+    $categoryTestName = 'Test';
+    $categoryTest->setName($categoryTestName);
+
+    $categoryId = $categoryDao->insertCategory($categoryTest);
+
+    $categoriesSelected = $categoryDao->selectAllCategories();
+
+    $this->assertTrue(is_array($categoriesSelected));
+    $this->assertNotNull($categoriesSelected[0]->getName());
+
+    $categoryDao->deleteCategoryById($categoryId);
+  }
+}
