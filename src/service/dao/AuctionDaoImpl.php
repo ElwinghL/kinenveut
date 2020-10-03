7618c52ea6529341eefe27ece3e6523cbf406277
OphelieTrd <57994130+OphelieTrd@users.noreply.github.com>
2020-10-03 15:13:11 +0200

    Update AuctionDaoImpl.php
    Par défaut, l'enchère est à l'état 0 :)

--- src/service/dao/AuctionDaoImpl.php
+++ src/service/dao/AuctionDaoImpl.php
@@ -48,7 +48,7 @@ class AuctionDaoImpl implements IAuctionDao
 
   public function insertAuction(AuctionModel $auction):?int
   {
-    $request = db()->prepare("INSERT INTO Auction(name, description, basePrice, reservePrice, pictureLink, startDate, duration, auctionState, sellerId, privacyId, categoryId) VALUES (?, ?, ?, ?, ' ', ?, ?, -1, ?, ?, ?)");
+    $request = db()->prepare("INSERT INTO Auction(name, description, basePrice, reservePrice, pictureLink, startDate, duration, auctionState, sellerId, privacyId, categoryId) VALUES (?, ?, ?, ?, ' ', ?, ?, 0, ?, ?, ?)");
 
     $request->execute([$auction->getName(), $auction->getDescription(), $auction->getBasePrice(), $auction->getReservePrice(), /*$auction->getPictureLink(),*/ $auction->getStartDate(), $auction->getDuration(), $auction->getSellerId(), $auction->getPrivacyId(), $auction->getCategoryId()]);
 
