1ce7ec2dbceea44413981d2f0106297f101202d2
Riddsaw <axlds98@gmail.com>
2020-10-04 23:52:20 +0200

    Ajout visu admin sur page enchere management + modif cdn bootstrap et header

--- src/view/page-header.php
+++ src/view/page-header.php
@@ -1,25 +1,28 @@
 <div class="container page-header-custom">
     <div class="row row-vertically-centered">
         <div class="col-md-10">
-            <a href="http://localhost/kinenveut/"><img src="resources/logo.png" width="158" height="143"></a>
+            <a href="?r=account"><img src="resources/logo.png" width="158" height="143"></a>
         </div>
         <div class="col-md-2">
-            <div class="dropdown">
-                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    Menu
-                    <span class="caret"></span>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li><a href="?r=account">Mon compte</a></li>
-                    <li><a href="?r=auction">Mes enchères</a></li>
-                    <li><a href="?r=myAuction">Mes ventes</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="?r=user">Gestion des utilisateurs</a></li>
-                    <li><a href="?r=auction/alerte">Gestion des enchères</a></li>
-                    <li><a href="?r=categorie">Gestion des catégories</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="?r=login">Déconnexion</a></li>
-                </ul>
+            <div class="dropdown" aria-labelledby="dropdownMenu1">
+                <div class="row">
+                    
+                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Menu
+                        <span class="caret"></span>
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="?r=account">Mon compte</a>
+                        <a class="dropdown-item" href="?r=auction">Mes enchères</a>
+                        <a class="dropdown-item" href="?r=myAuction">Mes ventes</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="?r=user">Gestion des utilisateurs</a>
+                        <a class="dropdown-item" href="?r=auctionManagement">Gestion des enchères</a>
+                        <a class="dropdown-item" href="?r=categorie">Gestion des catégories</a>
+                    
+                    </div>
+                    <input type="button" class="btn-secondary" value="Déconnexion">
+                </div>
             </div>
         </div>
     </div>
