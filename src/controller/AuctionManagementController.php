737d724433b88bf655c9dca6fdb3301b9f0beb20
bastien chanez <bastien.chanez@gmail.com>
2020-10-05 00:14:47 +0200

    format

--- src/controller/AuctionManagementController.php
+++ src/controller/AuctionManagementController.php
@@ -9,10 +9,10 @@ class AuctionManagementController extends Controller
   public function index()
   {
     $auctionBo = App_BoFactory::getFactory()->getAuctionBo();
-    $auctions=$auctionBo->selectAllAuctionsByAuctionState(0);
+    $auctions = $auctionBo->selectAllAuctionsByAuctionState(0);
     $data = [
       'auctions'=> $auctions
     ];
-    $this->render('index',$data);
+    $this->render('index', $data);
   }
 }
