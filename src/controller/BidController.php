29b2d9d083cd395860a6ffa7bf35653cd41dfb1c
OphelieTrd <ofeely-56@hotmail.fr>
2020-10-04 18:12:39 +0200

    Ajout de l'interface pour enchérir ou voir le détaili d'une enchère

---
+++ src/controller/BidController.php
@@ -0,0 +1,42 @@
+<?php
+
+class BidController extends Controller
+{
+  public function __construct()
+  {
+  }
+
+  public function index()
+  {
+    $auctionId = $_GET['auctionId'];
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $this->addBid($auctionId);
+    }
+
+    $auctionBo = App_BoFactory::getFactory()->getAuctionBo();
+    $auction = $auctionBo->selectAuctionByAuctionId($auctionId);
+
+    $data = [
+      'auction'=> $auction
+    ];
+
+    $this->render('index', $data);
+  }
+
+  private function addBid($auctionId) : int
+  {
+    $_POST['bidPrice'];
+
+    $newBid = new BidModel();
+    $newBid
+            ->setBidPrice($_POST['bidPrice'])
+            ->setBidderId(1) //todo : get user id (bidderId)
+            ->setObjectId($auctionId);
+
+    $bidHistoryBo = App_BoFactory::getFactory()->getBidHistoryBo();
+    $bidId = $bidHistoryBo->insertBid($newBid);
+
+    return $bidId;
+  }
+}
