341dbfdd97cba8a347b9d2309b2f58c1377dabe9
OphelieTrd <ofeely-56@hotmail.fr>
2020-10-04 18:09:31 +0200

    Ajout des éléments dans la liste de catégories + ajout du lien d'accès à l'enchère

--- src/view/home/index.php
+++ src/view/home/index.php
@@ -1,6 +1,6 @@
 <?php
-//$categoryList = $data['categoryList'];
-$auctionList = $data['auctionList'];
+  $categoryList = $data['categoryList'];
+  $auctionList = $data['auctionList'];
 ?>
 
 <?php include_once 'src/view/page-header.php' ?>
@@ -24,7 +24,14 @@ $auctionList = $data['auctionList'];
             <div class="form-group">
                 <label for="category">Catégorie</label>
                 <select id="category" class="form-control">
-                    <option>Toute catégorie</option>
+                    <option selected>Toutes</option>
+                    <?php if( sizeof($categoryList) > 0):?>
+                        <?php foreach ($categoryList as $oneCategory): ?>
+                            <option value="<?php echo $oneCategory->getId(); ?>">
+                                <?php echo $oneCategory->getName(); ?>
+                            </option>
+                        <?php endforeach;?>
+                    <?php endif; ?>
                 </select>
             </div>
         </div>
@@ -45,18 +52,18 @@ $auctionList = $data['auctionList'];
     <div class="row">
         <!-- BEGIN PRODUCTS -->
         <?php foreach ($auctionList as $auction) : ?>
-            <a class="col-md-3 col-sm-6 auction-custom" href=<?php echo "?auctionId=".$auction->getId() ?>>
+            <a class="col-md-3 col-sm-6 auction-custom" href=<?php echo '?r=bid/index&auctionId=' . $auction->getId() ?>>
                 <div class="thumbnail text-center">
                     <h4 class="text-danger auction-title-custom"> <?php echo $auction->getName(); ?> </h4>
-                    <p>Expiration : <?php echo date('d/m/Y h:m', strtotime($auction->getStartDate().' + '.$auction->getDuration().' days')); ?></p>
+                    <p>Expiration : <?php echo date('d/m/Y h:m', strtotime($auction->getStartDate() . ' + ' . $auction->getDuration() . ' days')); ?></p>
                     <hr class="line" />
                     <div class="row">
                         <div class="col-md-12">
-                        <?php if ($auction->getBestBid()->getbidPrice() !== null) { 
-                            echo '<p class="bg-danger">Dernière offre : '.$auction->getBestBid()->getbidPrice().'€</p>';
-                        } else { 
-                            echo '<p class="bg-danger">Prix de base : '.$auction->getBasePrice().'€</p>';
-                        } ?>
+                        <?php if ($auction->getBestBid()->getbidPrice() !== null) {
+  echo '<p class="bg-danger">Dernière offre : ' . $auction->getBestBid()->getbidPrice() . '€</p>';
+} else {
+  echo '<p class="bg-danger">Prix de base : ' . $auction->getBasePrice() . '€</p>';
+} ?>
                         </div>
                     </div>
                 </div>
