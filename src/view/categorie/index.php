89a5540f884df77c2caa78047550a2f94d3fdfbf
bastien chanez <bastien.chanez@gmail.com>
2020-10-06 20:31:21 +0200

    fix bug display error field

--- src/view/categorie/index.php
+++ src/view/categorie/index.php
@@ -1,23 +1,23 @@
 <?php
-    $categoryList = $data['categoryList'];
+$categoryList = $data['categoryList'];
 ?>
 
 <?php include_once 'src/view/page-header.php' ?>
 
 <div class="container">
-    <h2>
-        Gestion des catégories
-    </h2>
+  <h2>
+    Gestion des catégories
+  </h2>
 
-    <div class="col-12">
-        <?php if (sizeof($categoryList) > 0):?>
-        <ul class="list-group">
-            <?php foreach ($categoryList as $oneCategory): ?>
-                <li id="category_<?php echo $oneCategory->getId();?>" class="list-group-item">
-                    <?php echo utf8_encode($oneCategory->getName());?>
-                </li>
-        <?php endforeach;?>
-        </ul>
-        <?php endif;?>
-    </div>
+  <div class="col-12">
+    <?php if (sizeof($categoryList) > 0) : ?>
+      <ul class="list-group">
+        <?php foreach ($categoryList as $oneCategory) : ?>
+          <li id="category_<?php echo $oneCategory->getId(); ?>" class="list-group-item">
+            <?php echo utf8_encode($oneCategory->getName()); ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+  </div>
 </div>
