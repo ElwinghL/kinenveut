86ab8e62e5131e9650cf2e9f2a161681fec537ba
bastien chanez <bastien.chanez@gmail.com>
2020-10-08 12:16:41 +0200

    remove useless function

--- src/controller/AccountController.php
+++ src/controller/AccountController.php
@@ -77,18 +77,4 @@ class AccountController extends Controller
       return ['redirect', '?r=home'];
     }
   }
-
-  public function info(): array
-  {
-    $userId = parameters()['id'];
-    $userBo = App_BoFactory::getFactory()->getUserBo();
-
-    $user = $userBo->selectUserByUserId($userId);
-
-    $data = [
-      'user' => $user
-    ];
-
-    return['render', 'index', $data];
-  }
 }
