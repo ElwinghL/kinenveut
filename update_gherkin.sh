73eb020d333979acd86e934c1c1d499354c283bd
bastien chanez <bastien.chanez@gmail.com>
2020-10-14 20:04:05 +0200

    update script

--- update_gherkin.sh
+++ update_gherkin.sh
@@ -5,7 +5,7 @@ do
   nameContext=$( echo $( echo $( echo "$eachfile"| tr -d _) | cut -d"/" -f4) | cut -d"." -f1)Context
   if [ -z "$(cat behat.yml | grep "$nameContext")" ]
   then
-    sed -i '/contexts:/a \ \ \ \ \ \ \ \ - '$nameContext'' behat.yml
+    sed -i '/contexts:/a \ \ \ \ \ \ \ \ - '$nameContext behat.yml
   fi
 done
 vendor/bin/behat --init
@@ -13,6 +13,9 @@ for eachfile in $(ls ./test/resources/*.feature)
 do
   nameContext=$( echo $( echo $( echo "$eachfile"| tr -d _) | cut -d"/" -f4) | cut -d"." -f1)Context
   eachfile=$(echo "$eachfile" | cut -c 3-)
-  vendor/bin/behat --dry-run --append-snippets --colors --snippets-for=$nameContext $eachfile
-
+  if ! vendor/bin/behat --dry-run --append-snippets --colors --snippets-for=$nameContext $eachfile;
+  then
+    sed -i '/- '$nameContext'/d' behat.yml
+    rm $eachfile
+  fi
 done
