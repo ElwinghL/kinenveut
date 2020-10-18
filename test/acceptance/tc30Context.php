ea9a79e826b11b0df42a446b6ae1f277797dd9cd
bastien chanez <bastien.chanez@gmail.com>
2020-10-18 16:10:48 +0200

    tc30

--- test/acceptance/tc30Context.php
+++ test/acceptance/tc30Context.php
@@ -112,7 +112,57 @@ class tc30Context implements Context
      */
   public function lutilisateurEstDejaInscrit()
   {
-    throw new PendingException();
+    $session = Universe::getUniverse()->getSession();
+    $session->visit('http://localhost/kinenveut/');
+    if ($session->getStatusCode() !== 200) {
+      throw new Exception('status code is not 200');
+    }
+    if ($session->getCurrentUrl() !== 'http://localhost/kinenveut/?r=login') {
+      throw new Exception('url is not "http://localhost/kinenveut/?r=login"');
+    }
+
+    $session->getPage()->find(
+      'css',
+      'a[href="?r=registration"]'
+    )->click();
+
+    if ($session->getStatusCode() !== 200) {
+      throw new Exception('status code is not 200');
+    }
+    if ($session->getCurrentUrl() !== 'http://localhost/kinenveut/?r=registration') {
+      throw new Exception('url is not "http://localhost/kinenveut/?r=registration"');
+    }
+
+    $session = Universe::getUniverse()->getSession();
+    $user = Universe::getUniverse()->getUser();
+    $session->getPage()->find(
+      'css',
+      'input[name="firstName"]'
+    )->setValue($user->getFirstName());
+    $session->getPage()->find(
+      'css',
+      'input[name="lastName"]'
+    )->setValue($user->getLastName());
+    $session->getPage()->find(
+      'css',
+      'input[name="birthDate"]'
+    )->setValue($user->getBirthDate()->format('d/m/Y'));
+    $session->getPage()->find(
+      'css',
+      'input[name="email"]'
+    )->setValue($user->getEmail());
+    $session->getPage()->find(
+      'css',
+      'input[name="password"]'
+    )->setValue($user->getPassword());
+
+    $session = Universe::getUniverse()->getSession();
+    $session->getPage()->find(
+      'css',
+      'input[type="submit"]'
+    )->click();
+
+    Universe::getUniverse()->setCanDelete(['user'=>true]);
   }
 
   /**
@@ -120,6 +170,18 @@ class tc30Context implements Context
    */
   public function lutilisateurRecoitUnMessageDerreurLuiIndiquantQueLadresseMailQuilASaisieEstDejaUtilisee()
   {
-    throw new PendingException();
+    $session = Universe::getUniverse()->getSession();
+    if ($session->getStatusCode() !== 200) {
+      throw new Exception('status code is not 200');
+    }
+    if ($session->getCurrentUrl() !== 'http://localhost/kinenveut/?r=registration/register') {
+      throw new Exception('url is not "http://localhost/kinenveut/?r=registration/register"');
+    }
+    if ($session->getPage()->find(
+      'css',
+      '.invalid-feedback.d-block'
+    )->getText() != 'L\'adresse mail est déjà utilisée par un autre utilisateur') {
+      throw new Exception('There is not an error');
+    }
   }
 }
