419b8a34f8c7af429b3348da4eadf5850485b522
OphelieTrd <ofeely-56@hotmail.fr>
2020-10-18 17:46:40 +0200

    tc114Context.php

--- test/acceptance/tc114Context.php
+++ test/acceptance/tc114Context.php
@@ -24,6 +24,7 @@ class tc114Context implements Context
    */
   public function lutilisateurEstEnregistreSurLeSite()
   {
+    //Todo : à supprimer car compris dans le "est connecté"
     throw new PendingException();
   }
 
@@ -32,6 +33,75 @@ class tc114Context implements Context
    */
   public function lutilisateurRechercheUneVentePriveeExistantDansLaBaseDeDonnees()
   {
-    throw new PendingException();
+      /*Let's add an auction to the current user, as seen in tc71*/
+      $session = Universe::getUniverse()->getSession();
+      $user = Universe::getUniverse()->getUser();
+      $auction = new AuctionModel();
+
+      if ($user->getId() == null or $user->getId() < 1) {
+          $userDao = App_DaoFactory::getFactory()->getUserDao();
+          $user = $userDao->selectUserByEmail(Universe::getUniverse()->getUser()->getEmail());
+          Universe::getUniverse()->getUser()->setId($user->getId());
+          $user = Universe::getUniverse()->getUser();
+      }
+
+      $auction
+          ->setName('Objet test')
+          ->setDescription('Ceci est une enchère insérée lors de tests.')
+          ->setBasePrice(3)
+          ->setReservePrice(7)
+          ->setDuration(7)
+          ->setSellerId($user->getId())
+          ->setPrivacyId(1)
+          ->setCategoryId(1)
+          ->setStartDate(new DateTime());
+
+      Universe::getUniverse()->setAuction($auction);
+
+      visitCreateAuction($session);
+      createAuction($session, $auction);
+
+      disconnect($session);
+
+      /*Connection as Admin*/
+      $userAdmin = new UserModel();
+      $userAdmin
+          ->setEmail('admin@kinenveut.fr')
+          ->setPassword('password');
+
+      Universe::getUniverse()->setUser2($userAdmin);
+
+      connect($session, $userAdmin);
+
+      visitAuctionManagement($session);
+
+      $auctionDao = App_DaoFactory::getFactory()->getAuctionDao();
+      $userAuctions = $auctionDao->selectAllAuctionsBySellerId($user->getId());
+
+      if (count($userAuctions) == 1) {
+          $auction->setId($userAuctions[0]->getId());
+      } else {
+          throw new Exception('A problem happenned while create an auction');
+      }
+
+      /*Click to accept the prevent created auction*/
+      $url = 'http://localhost/kinenveut/?r=auctionManagement/validate&id=' . $auction->getId();
+      $session->visit($url);
+      checkUrl($session, $url);
+
+      disconnect($session);
+
+      connect($session, $user);
+
+      /*Ok, great ! Now the user can search is own auction*/
+
+      $session->getPage()->find(
+          'css',
+          'input[name="searchInput"]'
+      )->setValue($auction->getName());
+      $session->getPage()->find(
+          'css',
+          '#privacyId'
+      )->selectOption(1);
   }
 }
